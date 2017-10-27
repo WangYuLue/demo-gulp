79f14036b5ae519bc496ceb97762d05db03e35f9
wanger <1205722784@qq.com>
2017-10-27 14:52:21 +0800

    demo

---
+++ demo01/js/app.js
@@ -0,0 +1,71 @@
+//验证身份证的合法性
+function IdentityCodeValid(code) { 
+    function cap (str){    //如果身份证验证中有x,则变为X再做判断；
+        var temp = str.split("");
+        temp.forEach(function(i,index){if(i==='x'){temp[index]="X"}});return temp.join("");
+    }
+
+    var code = cap(code);
+    var copycode = code ;
+    var city={11:"北京",12:"天津",13:"河北",14:"山西",15:"内蒙古",21:"辽宁",22:"吉林",23:"黑龙江 ",31:"上海",32:"江苏",33:"浙江",34:"安徽",35:"福建",36:"江西",37:"山东",41:"河南",42:"湖北 ",43:"湖南",44:"广东",45:"广西",46:"海南",50:"重庆",51:"四川",52:"贵州",53:"云南",54:"西藏 ",61:"陕西",62:"甘肃",63:"青海",64:"宁夏",65:"新疆",71:"台湾",81:"香港",82:"澳门",91:"国外 "};
+    var tip = "";
+    var pass= true;
+    
+    if(!code || !/^\d{6}(18|19|20)?\d{2}(0[1-9]|1[012])(0[1-9]|[12]\d|3[01])\d{3}(\d|X)$/i.test(code)){
+        tip = "身份证号格式错误";
+        pass = false;
+    }
+    
+    else if(!city[code.substr(0,2)]){
+        tip = "地址编码错误";
+        pass = false;
+    }
+    else{
+        //18位身份证需要验证最后一位校验位
+        if(code.length == 18){
+            code = code.split('');
+            //∑(ai×Wi)(mod 11)
+            //加权因子
+            var factor = [ 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2 ];
+            //校验位
+            var parity = [ 1, 0, 'X', 9, 8, 7, 6, 5, 4, 3, 2 ];
+            var sum = 0;
+            var ai = 0;
+            var wi = 0;
+            for (var i = 0; i < 17; i++)
+            {
+                ai = code[i];
+                wi = factor[i];
+                sum += ai * wi;
+            }
+            var last = parity[sum % 11];
+            if(parity[sum % 11] != code[17]){
+                tip = "校验位错误";
+                pass =false;
+            }
+        }
+    }
+
+    //判断出生日期是否合法
+    function check(date){    //date 传 例如：2013-01-01、2013/01/01、2013/01/32、2013/02/29
+        return (new Date(date).getDate()==date.substring(date.length-2));
+    }
+    function getDate(code){    //得到类似2013-01-01、2013/01/01、2013/01/32、2013/02/29的日期格式
+        var date = '' ;
+        if(code.length===18){
+             date = code.substring(6,14)
+        }else if(code.length===15){
+             date = '19'+code.substring(6,12)
+        }else{
+            return '';
+        }
+        date = date.substr(0,4)+'-'+date.substr(4,2)+'-'+date.substr(6,2);
+        return date;
+    }
+    if(!check(getDate(copycode))){
+        pass = false ;
+        tip = "日期格式不正确"
+    }
+//   if(!pass) alert(tip);
+    return pass;
+}
---
+++ demo01/readme.md
@@ -0,0 +1,5 @@
+### 压缩js文件
+
+>cd demo01
+npm install
+gulp minify
