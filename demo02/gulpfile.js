3af9a41311e819819e8704d39799457d87cb0408
wanger <1205722784@qq.com>
2017-10-27 15:03:18 +0800

    demo

--- demo02/gulpfile.js
+++ demo02/gulpfile.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify');
+    jshint = require('gulp-jshint');
+    concat = require('gulp-concat');
 
-gulp.task('minify', function () {
-   gulp.src('js/app.js')
-      .pipe(uglify())
-      .pipe(gulp.dest('build'))
+gulp.task('dealJS', function () {
+    gulp.src('js/*.js')
+         .pipe(jshint())
+         .pipe(jshint.reporter('default'))
+         .pipe(uglify())
+         .pipe(concat('app.js'))
+         .pipe(gulp.dest('build'));
 });
---
+++ demo02/readme.md
@@ -0,0 +1,5 @@
+### 检查js语法，压缩并合并js文件
+
+>cd demo02
+npm install
+gulp dealJS
